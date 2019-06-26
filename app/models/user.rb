6decd3e8b7f76882bb9145e83f4543cfb9e7d7f4
sushmitha02 <sushmithahgowda27@gmail.com>
2019-06-26 15:54:42 +0530

    Added guest user for current_user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+  :recoverable, :rememberable, :trackable, :validatable
+
   validates_presence_of :name
+
   def first_name
   	self.name.split.first
   end
 
   def last_name
-   	self.name.split.last
+    self.name.split.last
   end
+
 end
