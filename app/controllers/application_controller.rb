178731fdcc0caf7c270a3926f16f08f3decc3a25
sushmitha02 <sushmithahgowda27@gmail.com>
2019-06-28 11:19:13 +0530

    Adding copyright.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < ActionController::Base
 	include CurrentUserConcern
 	include DefaultPageContent
 
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = DevcampViewTool::Renderer.copyright 'Sushmitha Gowda', 'All rights reserved'
+	end
+
+	module DevcampViewTool
+		class Renderer
+			def self.copyright name,msg
+				"&copy; #{Time.now.year} | <b>#{name}<b> #{msg}".html_safe
+			end
+		end
+	end
+
 end
